7563fdc80660a14c576ceac7ba8eacef9245572b
k1863 <57972610+k1863@users.noreply.github.com>
2021-02-10 23:53:08 -0300

    heroku deploy

--- client/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const handleSaveDataToLocal = (data) => {
-  localStorage.setItem("allLocalPhrases", JSON.stringify(data));
-};
-
-export const handleLastPhraseToLocal = (data) => {
-  localStorage.setItem("localNewPhrase", JSON.stringify(data));
-};
